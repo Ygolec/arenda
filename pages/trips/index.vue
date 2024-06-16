d790c33f94608ac01d01a2ca308454e64f850dd2
Daniil <thekevindit@gmail.com>
2024-06-16 22:40:51 +0300

    Добавление страницы просмотра своих бронирований, первый черновик

---
+++ pages/trips/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-sheet class="ma-3">
+    <tabs-of-trips/>
+  </v-sheet>
+</template>
+<script setup lang="ts">
+
+
+import TabsOfTrips from "~/components/trips/tabs-of-trips.vue";
+</script>
+
+<style scoped>
+
+</style>
